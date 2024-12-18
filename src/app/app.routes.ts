3add7bb984fbd896cb1a18f5ab9a72492590bd82
MrNizzy <nizzy0128@gmail.com>
2024-12-18 15:18:56 -0500

    Enhance TypeScript configuration and routing with environment variables
    - Added path mappings in tsconfig.json for cleaner imports across the application.
    - Updated app.routes.ts to utilize the new path mappings and include the application name in the title.
    - Defined environment variables in environment.development.ts and environment.ts for production and development settings, including the app name.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
+import { environment } from '@env/environment';
 
 export const routes: Routes = [
   {
     path: '',
     loadComponent: () =>
-      import('./pages/home/home.component').then((m) => m.HomeComponent),
-    title: 'Inicio',
+      import('@pages/home/home.component').then((m) => m.HomeComponent),
+    title: `Inicio - ${environment.appName}`,
   },
 ];
